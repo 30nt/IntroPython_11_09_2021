7ba0a99770fe5fb25d2a8bc05b56467a18b8e636
hillel <zontovv@ouylook.com>
2021-09-25 17:37:46 +0300

    finalize lesson2.py

--- lesson2.py
+++ lesson2.py
@@ -1,26 +1,57 @@
-# логический оператор if
-# тернарный оператор
-# строки, методы строк
-# циклы for, while
-
-# приведение типов
-value = ""
-value_bool = bool(value)  # Всегда True, кроме значения "" --> False
-print(value_bool)
-
-value = 0.0
-value_bool = bool(value)  # Всегда True, кроме значения 0.0 --> False  0.00000....001 (400+ нулей)
-print(value_bool)
-
-value = 0
-value_bool = bool(value)  # Всегда True, кроме значения 0 --> False
-print(value_bool)
-###########################################################################
-# if условие_1:
-#     блок, если ДА (условие_1)
-# elif условие_2:
-#     блок, если ДА (условие_2)
-# ...
+# # логический оператор if
+# # тернарный оператор
+# # строки, методы строк
+# # циклы for, while
+#
+# # приведение типов
+# value = ""
+# value_bool = bool(value)  # Всегда True, кроме значения "" --> False
+# print(value_bool)
+#
+# value = 0.0
+# value_bool = bool(value)  # Всегда True, кроме значения 0.0 --> False  0.00000....001 (400+ нулей)
+# print(value_bool)
+#
+# value = 0
+# value_bool = bool(value)  # Всегда True, кроме значения 0 --> False
+# print(value_bool)
+# ###########################################################################
+# # if условие_1:
+# #     блок, если ДА (условие_1)
+# # elif условие_2:
+# #     блок, если ДА (условие_2)
+# # ...
+# # else:
+# #     блок, если НЕТ
+#
+#
+# temp = -0.1
+#
+# if temp > 0:
+#     print("Можно шапку не надевать ))")
+#     print("temp:", temp)
+# else:
+#     print("Надень шапку!")
+#     print("End of else")
+#
+#
+# case = input("Кинь кубик:")  # всегда str!!!
+# # case = int(case)
+# print(case, type(case))
+# if case == "6":
+#     print("Победил Вася!")
+# elif case == "1":
+#     print("Победил Петя!")
+# else:
+#     print("Все проиграли! ))")
+#
+# from random import randint
+# case = randint(1, 6)
+# print("Выпало число:", case)
+# if case == 6:
+#     print("Победил Вася!")
+# elif case == 1:
+#     print("Победил Петя!")
 # else:
 #     блок, если НЕТ
 
@@ -105,25 +136,22 @@ index = 5
 new_str = my_str_1[: index] + "K" + my_str_1[index + 1:]  # замена символа на конкретном месте в строке
 
 
-new_str = my_str_1[1:-1:3]  # 3 - шаг среза
-new_str = my_str_1[::2]  # символы на четных местах в строке
-new_str = my_str_1[1::2]  # символы на нечетных местах в строке
-new_str = my_str_1[::-1]  # разворот строки
-print(new_str)
+####################################################################
 
-my_str_1 = "IIIIaaye'm Qwerty"
+# while условие:
+#     блок, если ДА
 
-if my_str_1[-1] == "a":
-    print(f"'a' on last position in {my_str_1}")
-else:
-    print(f"'a' not on last position in {my_str_1}")
 
-for symbol in my_str_1:  # строка - итерируемый объект
-    if (symbol.lower() not in "eyuioa") and symbol.isupper():
-        print(symbol)
+count = 0
+while count < 10:
+    print("This is while loop", count)
+    count += 1
+########################################################
+count = 0
+do_loop = True
 
-for symbol in my_str_1:
-    print(f"symbol: '{symbol}' --> {ord(symbol)}")
-
-for index in range(ord("a"), ord('z') + 1, 2):
-    print(f"index: {index} --> '{chr(index)}'")
+while do_loop:
+    print("This is while loop", count)
+    count += 1
+    if count >= 10:
+        do_loop = False
