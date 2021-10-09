cfb01d6ec44e9756d17ddbeaabfdc64473cb89bd
hillel <zontovv@ouylook.com>
2021-10-09 17:39:16 +0300

    add lesson4

--- lesson4.py
+++ lesson4.py
@@ -5,13 +5,13 @@
 Вывод на экран:
 2
 """
-# my_str = "blablacarblablacar"
-# my_symbol="bla"
-#
-# result = my_str.count(my_symbol)
-# # new_str = my_str.replace(my_symbol, '')
-# # result = (len(my_str) - len(new_str)) // len(my_symbol)
-# print(result)
+my_str = "blablacarblablacar"
+my_symbol="bla"
+
+result = my_str.count(my_symbol)
+# new_str = my_str.replace(my_symbol, '')
+# result = (len(my_str) - len(new_str)) // len(my_symbol)
+print(result)
 
 
 """
@@ -22,17 +22,17 @@
 bla
 bla
 """
-# my_str = "blablacar"
-# my_symbol="bla"
-#
-# my_symbol_count = my_str.count(my_symbol)
-#
-# result = f"{my_symbol}\n" * my_symbol_count
-# result = result.strip()
-# print(result)
-#
-# for _ in range(my_symbol_count):
-#     print(my_symbol)
+my_str = "blablacar"
+my_symbol="bla"
+
+my_symbol_count = my_str.count(my_symbol)
+
+result = f"{my_symbol}\n" * my_symbol_count
+result = result.strip()
+print(result)
+
+for _ in range(my_symbol_count):
+    print(my_symbol)
 
 
 """
@@ -44,15 +44,15 @@ bla
 Вывод на экран:
 6
 """
-# my_str = "bla BLA car"
-# lower_str = my_str.lower()
-# unique_symbols = []
-#
-# for symbol in lower_str:
-#     if symbol not in unique_symbols:
-#         unique_symbols.append(symbol)
-# unique_symbols_count = len(unique_symbols)
-# print(unique_symbols_count)
+my_str = "bla BLA car"
+lower_str = my_str.lower()
+unique_symbols = []
+
+for symbol in lower_str:
+    if symbol not in unique_symbols:
+        unique_symbols.append(symbol)
+unique_symbols_count = len(unique_symbols)
+print(unique_symbols_count)
 
 
 """
@@ -61,19 +61,19 @@ bla
 Заполнить my_list символами из my_str, 
 которые стоят на четных местах в строке (считаем с 0)
 """
-# val_1 = 123
-# val_2 = 100
-# val_1 += val_2
-# print(val_1)
+val_1 = 123
+val_2 = 100
+val_1 += val_2
+print(val_1)
 
-# my_str = "xdkzkzjdvkzsdbvkxbzvkhbdzsfh"
-# my_list = []
-# print(id(my_list), my_list)
-# new_str = my_str[::2]
-# # for symbol in new_str:
-# #     my_list.append(symbol)
-# my_list += list(new_str)  # НЕ ТО ЖЕ САМОЕ, ЧТО my_list = my_list + list(new_str)
-# print(id(my_list), my_list)
+my_str = "xdkzkzjdvkzsdbvkxbzvkhbdzsfh"
+my_list = []
+print(id(my_list), my_list)
+new_str = my_str[::2]
+# for symbol in new_str:
+#     my_list.append(symbol)
+my_list += list(new_str)  # НЕ ТО ЖЕ САМОЕ, ЧТО my_list = my_list + list(new_str)
+print(id(my_list), my_list)
 """
 5)
 Дана строка my_str, список str_index целых чисел в диапазоне от 
@@ -81,65 +81,65 @@ bla
 Заполнить my_list символами из my_str, которые стоят на местах с 
 индексами из str_index
 """
-# from string import ascii_lowercase as alphabet
-#
-# my_str = alphabet
-# str_index = [7, 8, 11, 11, 4, 11]
-# my_list = []
-# for index in str_index:
-#     symbol = my_str[index]
-#     my_list.append(symbol)
-# print(my_list)
+from string import ascii_lowercase as alphabet
+
+my_str = alphabet
+str_index = [7, 8, 11, 11, 4, 11]
+my_list = []
+for index in str_index:
+    symbol = my_str[index]
+    my_list.append(symbol)
+print(my_list)
 
 """
 6)
 Дано целое число (тип int). Определить сколько цифр в этом числе.
 """
 
-# number = 128973816581735168456287359129834092876582634
-# digit_count = len(str(number))
-# print(digit_count)
+number = 128973816581735168456287359129834092876582634
+digit_count = len(str(number))
+print(digit_count)
 
 
 """
 7)
 Дано целое число (тип int). Определить наибольшую цифру в этом числе.
 """
-# number = 2873816581735168456287351283402876582634
-# max_symbol = max(str(number))
-# print(max_symbol)
+number = 2873816581735168456287351283402876582634
+max_symbol = max(str(number))
+print(max_symbol)
 
 
 """
 8)
 Дано целое число (тип int). Составить число (int) с цифрами в обратном порядке.
 """
-# number = 2873816581735168456287351283402876582634
-#
-# # result_number = int(str(number)[::-1])
-#
-# numb_str = str(number)
-# numb_str = numb_str[::-1]
-# result_number = int(numb_str)
-#
-# print(result_number)
+number = 2873816581735168456287351283402876582634
+
+# result_number = int(str(number)[::-1])
+
+numb_str = str(number)
+numb_str = numb_str[::-1]
+result_number = int(numb_str)
+
+print(result_number)
 
 """
 9)
 Дано целое число (тип int). Составить число с цифрами в порядке возрастания(убывания).
 """
-# number = 28738165817351684562873512834028765826340000000
-# numb_str = str(number)
-# sort_numb_list = sorted(numb_str, reverse=True)
-# new_number = "".join(sort_numb_list)
-# result = int(new_number)
-# print(result)
+number = 28738165817351684562873512834028765826340000000
+numb_str = str(number)
+sort_numb_list = sorted(numb_str, reverse=True)
+new_number = "".join(sort_numb_list)
+result = int(new_number)
+print(result)
 
 
-# my_list = [3, 6, 1, 8]
-# # my_list_sorted = sorted(my_list, reverse=True)
-# my_list.sort(reverse=True)
-# print(my_list)
+my_list = [3, 6, 1, 8]
+# my_list_sorted = sorted(my_list, reverse=True)
+my_list.sort(reverse=True)
+print(my_list)
 
 
 
@@ -150,15 +150,15 @@ my_list_2 через один, начиная с my_list_1.
 а) кол-во эл-тов одинаковое
 б) кол-во эл-тов разное. Оставшиеся дописать в конец.
 """
-# my_list_1 = [1, 2, 3]
-# my_list_2 = [10, 20, 30]
-# my_result = []
-#
-# for index in range(len(my_list_1)):
-#     # my_result.extend([my_list_1[index], my_list_2[index]])
-#     my_result.append(my_list_1[index])
-#     my_result.append(my_list_2[index])
-# print(my_result)
+my_list_1 = [1, 2, 3]
+my_list_2 = [10, 20, 30]
+my_result = []
+
+for index in range(len(my_list_1)):
+    # my_result.extend([my_list_1[index], my_list_2[index]])
+    my_result.append(my_list_1[index])
+    my_result.append(my_list_2[index])
+print(my_result)
 
 my_list_1 = [1, 2, 3, 4, 5, 6, 7, 8]
 my_list_2 = [10, 20, 30, 40, 50]
--- lesson4_2.py
+++ lesson4_2.py
@@ -1,45 +1,45 @@
 # ord(), chr()
-# print(ord("A"))
-# print(chr(34))
+print(ord("A"))
+print(chr(34))
 
 
 # Генераторы списков (list comprehension)
-# alphabet_list = []
-# for index_ascii in range(ord('a'), ord('z') + 1):
-#     alphabet_list.append(chr(index_ascii))
+alphabet_list = []
+for index_ascii in range(ord('a'), ord('z') + 1):
+    alphabet_list.append(chr(index_ascii))
 
-# alphabet_list = [chr(index_ascii) for index_ascii in range(ord('a'), ord('z') + 1)]
-# alphabet = "".join(alphabet_list)
-# print(alphabet)
-#
-# result = [x ** 2 for x in range(25)]
-# print(result)
+alphabet_list = [chr(index_ascii) for index_ascii in range(ord('a'), ord('z') + 1)]
+alphabet = "".join(alphabet_list)
+print(alphabet)
 
-# my_list = [12, -45, 23, 5, 0, 21, 900]
-#
-# res = [str(value) * 20 for value in my_list if value > 10]
-# res = []
-# for value in my_list:
-#     if value > 10:
-#         res.append(value)
+result = [x ** 2 for x in range(25)]
+print(result)
 
-# [print(line) for line in res]
-# for line in res:
-#     print(line)
+my_list = [12, -45, 23, 5, 0, 21, 900]
+
+res = [str(value) * 20 for value in my_list if value > 10]
+res = []
+for value in my_list:
+    if value > 10:
+        res.append(value)
+
+[print(line) for line in res]
+for line in res:
+    print(line)
 
 
 # множества (set) - изменяемый тип, только один представитель для какждого объекта, порядок не сохраняется
 
-# my_list = [1, "2", 3, 4, "5", "5", "1"]
-# my_list_unique = list(set(my_list))  # Убрать дубли в списке
-# my_set = set(my_list)
-# my_set.add(1000)
-# print(my_set, type(my_set))
+my_list = [1, "2", 3, 4, "5", "5", "1"]
+my_list_unique = list(set(my_list))  # Убрать дубли в списке
+my_set = set(my_list)
+my_set.add(1000)
+print(my_set, type(my_set))
 
 
-# my_str = "bla BLA carqqqq"
-# unique_symbols_count = len(set(my_str.lower()))
-# print(unique_symbols_count)
+my_str = "bla BLA carqqqq"
+unique_symbols_count = len(set(my_str.lower()))
+print(unique_symbols_count)
 
 my_str_1 = "sdlkjfgkzdfkzhdfkjvakjdfvbaydsgfaksjdasudvb"
 my_str_2 = "saudhflsdkzao;uh giy7gfsazhnvcahirhvsmcflaouewyhflia"
